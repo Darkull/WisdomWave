15a7b6859947575c9a5b05a72a094325cfe61ae8
Alif Hossain <alifhossain5577@gmail.com>
2023-09-28 08:43:42 +0600

    added functionality using javascrip

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const adviceNumber = document.querySelector(".advice-number");
+  const advice = document.querySelector(".advice");
+  const button = document.querySelector("button");
+
+  async function updateAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    if (response.ok) {
+      adviceNumber.textContent = "advice #" + data.slip.id;
+      advice.textContent = '"' + data.slip.advice + '"';
+    } else {
+      advice.textContent = "An error occured";
+      console.log(data);
+    }
+  }
+  button.addEventListener("click", updateAdvice);
+  updateAdvice();
+});
